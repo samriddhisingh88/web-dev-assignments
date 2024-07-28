d43d9ed6e50246025b67fe33319bde13b84bb8e2
samriddhisingh88 <103570979+samriddhisingh88@users.noreply.github.com>
2024-07-29 00:34:24 +0530

    my second commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,7 +1,7 @@
 /*
   Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
   What's Anagram?
-  MY NAME IS SAMRIDDHI
+  MY NAME IS SAMRIDDHI SINGH
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
